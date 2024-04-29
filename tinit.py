6549b79e3f9dec5bdaf98b6d4cc6f2468d654612
tinowitsch <martinekowitsch@chello.at>
2024-04-29 23:39:57 +0200

    Create tinit.py
    tensorflow

---
+++ tinit.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
