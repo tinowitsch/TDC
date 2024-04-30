5afadba3e97da3f76b86157f688911b101e1a970
tinowitsch <martinekowitsch@chello.at>
2024-04-30 23:59:12 +0200

    Update tinit.py
    tiny tensorflow changes

--- tinit.py
+++ tinit.py
@@ -1,8 +1,22 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = tf.keras.datasets.mnist
+myMatrix = np.array([(2,3,4),(2,3,4),(2,3,4)],dtype = 'int32')
+myMatrix2 = np.array([(1,1,1),(1,1,1),(1,1,1)],dtype = 'int32')
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+print (myMatrix)
+print (myMatrix2)
+
+myMatrix = tf.constant(myMatrix)
+myMatrix2 = tf.constant(myMatrix2)
+matrix_product = tf.matmul(myMatrix, myMatrix2)
+matrix_sum = tf.add(myMatrix,myMatrix2)
+matrix_3 = np.array([(2,7,2),(1,4,2),(9,0,2)],dtype = 'float32')
+print (matrix_3)
+
+with tf.Session() as sess:
+   result1 = sess.run(matrix_product)
+   result2 = sess.run(matrix_sum)
+
+print (result1)
+print (result2)
